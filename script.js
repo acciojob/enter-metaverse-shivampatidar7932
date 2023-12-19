b6cb51cd2bcefee2dceace82c13d600c837520b2
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-19 13:58:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ function updateStatus() {
+            // Get the "p" tag by its id
+            var statusElement = document.getElementById("status");
+	 statusElement.remove();
+
+            // Update the text content of the "p" tag
+            statusElement.textContent = "Entered Metaverse";
+
+            // Optionally, you can also create and append an h1 tag
+            var h1Element = document.createElement("h1");
+            h1Element.textContent = "Entered Metaverse";
+
+            // Append the h1 tag to the body or another container if needed
+            document.body.appendChild(h1Element);
+        }
