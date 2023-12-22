18c0a73d57e3ae2624aff6df1101cd728f863bfc
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-22 12:41:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function updateStatus() {
    const para=document.querySelector("p");
 	const h1=document.createElement("h1");
 	h1.innerText="Entered Metaverse";
+	h1.id="status"
 	para.remove();
 	document.body.before(h1);
 }
