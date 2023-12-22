f5a1aeaa4425dea4fadf46ddd5c96bc61cc243aa
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-22 12:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 function updateStatus() {
     // Get the "p" tag by its id
-    var statusElement = document.getElementById("status");
-
-    // Create a new "h1" element
-    var h1Element = document.createElement("h1");
-
-    // Copy the content and attributes from the "p" element to the new "h1" element
-    h1Element.textContent = statusElement.textContent;
-    // h1Element.id = statusElement.id;
-
-    // Replace the "p" element with the new "h1" element
-    statusElement.parentNode.replaceChild(h1Element, statusElement);
+   const para=document.querySelector("p");
+	const h1=document.createElement("h1");
+	h1.innerText="Entered Metaverse";
+	para.remove();
+	document.body.before(h1);
 }
